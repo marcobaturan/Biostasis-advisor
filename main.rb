55686b7af65bd415d0d8c7e63b9fb50c8c803563
Marco Baturan <marco.baturan@gmail.com>
2021-11-29 11:12:33 +0100

    Add transient logic to /result URL to erb file.

--- main.rb
+++ main.rb
@@ -8,25 +8,18 @@ end
 
 post '/result' do
   # Get ages from params, make average life expectancy.
-  longevity = (params[:maternalgrandmother].to_i + \
+  @longevity = (params[:maternalgrandmother].to_i + \
                params[:maternalgrandfather].to_i + params[:paternalgrandmother].to_i + \
                params[:paternalgrandfather].to_i + params[:mother].to_i + \
                params[:father].to_i + params[:Me].to_i + params[:lifeexpectancy].to_i) / 8
   # Calculate months from actual age.
-  age = params[:me].to_i
-  months = (longevity - age) * 12
+  @age = params[:me].to_i
+  @months = (@longevity - @age) * 12
   # Sum all the costs.
-  cost = params[:legal].to_i + params[:backup].to_i + \
+  @cost = params[:legal].to_i + params[:backup].to_i + \
          params[:SST].to_i + params[:facility].to_i
   # Calculate the monthly amount to save.
-  save = cost / months
+  @save = @cost / @months
 
-  "
-  <p>Your statistical average life expectancy is: #{longevity} years.</p>
-  <p>At your current age (#{age}) you have to save #{save} euros per month until</p>
-  <p>you are #{longevity} years old.</p>
-  <p>As a supplement to your life insurance to finance your biostasis.</p>
-  <p>Have a nice day.</p>
-  "
-
+  erb :result
 end
